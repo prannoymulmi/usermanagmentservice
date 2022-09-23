56fd3bedb37243b34d29aa9d0c990b7a992f96d2
prannoy <prannoy.mulmi@gmail.com>
2022-09-23 15:17:31 +0200

    removed stuff

--- src/main/java/com/prannoy/usermanagementservice/rest/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.prannoy.usermanagementservice.rest.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/prannoy/usermanagementservice/rest/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.prannoy.usermanagementservice.rest.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
